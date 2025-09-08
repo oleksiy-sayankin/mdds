453c4dfc0fdbd4251cfd573651e10d68c3193f1a
Oleksiy Oleksandrovych Sayankin <oleksiy.sayankin@gmail.com>
2025-09-08 17:48:09 +0300

    MDDS-71 Fix SlaeSolvingMethod to camel case

--- src/main/java/dto/TaskDTO.java
+++ src/main/java/dto/TaskDTO.java
@@ -15,5 +15,5 @@ public class TaskDTO {
   private Instant dateTime;
   private double[][] matrix;
   private double[] rhs;
-  private String SlaeSolvingMethod;
+  private String slaeSolvingMethod;
 }
