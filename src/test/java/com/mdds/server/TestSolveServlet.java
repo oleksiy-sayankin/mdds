e46e86a23bacf0bee5bc21c04f9d1d399f31474a
Oleksiy Oleksandrovych Sayankin <oleksiy.sayankin@gmail.com>
2025-09-20 22:30:22 +0300

    MDDS-111 Add Junits for SolveServlet, ServletHelper and CsvHelper

---
+++ src/test/java/com/mdds/server/TestSolveServlet.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2025 Oleksiy Oleksandrovych Sayankin. All Rights Reserved.
+ * Refer to the LICENSE file in the root directory for full license details.
+ */
+package com.mdds.server;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.mockito.Mockito.*;
+
+import com.mdds.queue.Queue;
+import com.mdds.storage.DataStorage;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestSolveServlet {
+  private SolveServlet servlet;
+  private HttpServletRequest request;
+  private HttpServletResponse response;
+  private ServletContext servletContext;
+  private Queue queue;
+  private DataStorage dataStorage;
+
+  @BeforeEach
+  void setUp() {
+    servlet = new SolveServlet();
+    request = mock(HttpServletRequest.class);
+    response = mock(HttpServletResponse.class);
+    servletContext = mock(ServletContext.class);
+    queue = mock(Queue.class);
+    dataStorage = mock(DataStorage.class);
+  }
+
+  @Test
+  void testDoPost() throws ServletException, IOException {
+    // Prepare matrix of SLAE
+    var expectedMatrix =
+        new String[][] {{"1.3", "2.2", "3.7"}, {"7.7", "2.1", "9.3"}, {"1.1", "4.8", "2.3"}};
+    var sb = new StringBuilder();
+    for (var row : expectedMatrix) {
+      sb.append(String.join(",", row));
+      sb.append(System.lineSeparator());
+    }
+    var isMatrix = new ByteArrayInputStream(sb.toString().getBytes(UTF_8));
+    var partMatrix = mock(Part.class);
+    when(partMatrix.getInputStream()).thenReturn(isMatrix);
+    when(request.getPart("matrix")).thenReturn(partMatrix);
+
+    // Prepare right hand side of SLAE
+    var expectedRhs = new String[] {"1.3", "2.2", "3.7"};
+    var isRhs =
+        new ByteArrayInputStream(String.join(System.lineSeparator(), expectedRhs).getBytes(UTF_8));
+    var partRhs = mock(Part.class);
+    when(partRhs.getInputStream()).thenReturn(isRhs);
+    when(request.getPart("rhs")).thenReturn(partRhs);
+
+    // Prepare solving method
+    when(request.getParameter("slaeSolvingMethod")).thenReturn("numpy_exact_solver");
+
+    // Prepare queue
+    when(request.getServletContext()).thenReturn(servletContext);
+    when(servletContext.getAttribute(AppContextListener.ATTR_TASK_QUEUE)).thenReturn(queue);
+
+    // Prepare data storage
+    when(request.getServletContext()).thenReturn(servletContext);
+    when(servletContext.getAttribute(AppContextListener.ATTR_DATA_STORAGE)).thenReturn(dataStorage);
+    var printWriter = mock(PrintWriter.class);
+    when(response.getWriter()).thenReturn(printWriter);
+    doNothing().when(printWriter).write(anyString());
+    servlet.doPost(request, response);
+    verify(response).setContentType("application/json");
+    verify(printWriter).write(contains("id"));
+  }
+}
