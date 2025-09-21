2a214dff60e04225403f1ce4e00c56fc9cbad1f2
Oleksiy Oleksandrovych Sayankin <oleksiy.sayankin@gmail.com>
2025-09-21 18:40:35 +0300

    MDDS-112 Implement JUnits for SolveServlet

--- src/test/java/com/mdds/server/TestSolveServlet.java
+++ src/test/java/com/mdds/server/TestSolveServlet.java
@@ -4,6 +4,8 @@
  */
 package com.mdds.server;
 
+import static jakarta.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
+import static jakarta.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.mockito.Mockito.*;
 
@@ -77,5 +79,174 @@ class TestSolveServlet {
     servlet.doPost(request, response);
     verify(response).setContentType("application/json");
     verify(printWriter).write(contains("id"));
+  }
+
+  @Test
+  void testDoPostNoMatrix() throws ServletException, IOException {
+    // Prepare right hand side of SLAE
+    var expectedRhs = new String[] {"1.3", "2.2", "3.7"};
+    var isRhs =
+        new ByteArrayInputStream(String.join(System.lineSeparator(), expectedRhs).getBytes(UTF_8));
+    var partRhs = mock(Part.class);
+    when(partRhs.getInputStream()).thenReturn(isRhs);
+    when(request.getPart("rhs")).thenReturn(partRhs);
+
+    // Prepare solving method
+    when(request.getParameter("slaeSolvingMethod")).thenReturn("numpy_exact_solver");
+
+    // Prepare queue
+    when(request.getServletContext()).thenReturn(servletContext);
+    when(servletContext.getAttribute(AppContextListener.ATTR_TASK_QUEUE)).thenReturn(queue);
+
+    // Prepare data storage
+    when(request.getServletContext()).thenReturn(servletContext);
+    when(servletContext.getAttribute(AppContextListener.ATTR_DATA_STORAGE)).thenReturn(dataStorage);
+    var printWriter = mock(PrintWriter.class);
+    when(response.getWriter()).thenReturn(printWriter);
+    doNothing().when(printWriter).write(anyString());
+    servlet.doPost(request, response);
+    verify(response).sendError(SC_BAD_REQUEST, "Missing matrix in request");
+  }
+
+  @Test
+  void testDoPostNoRhs() throws ServletException, IOException {
+    // Prepare matrix of SLAE
+    var expectedMatrix =
+        new String[][] {{"1.3", "2.2", "3.7"}, {"7.7", "2.1", "9.3"}, {"1.1", "4.8", "2.3"}};
+    var sb = new StringBuilder();
+    for (var row : expectedMatrix) {
+      sb.append(String.join(",", row));
+      sb.append(System.lineSeparator());
+    }
+    var isMatrix = new ByteArrayInputStream(sb.toString().getBytes(UTF_8));
+    var partMatrix = mock(Part.class);
+    when(partMatrix.getInputStream()).thenReturn(isMatrix);
+    when(request.getPart("matrix")).thenReturn(partMatrix);
+
+    // Prepare solving method
+    when(request.getParameter("slaeSolvingMethod")).thenReturn("numpy_exact_solver");
+
+    // Prepare queue
+    when(request.getServletContext()).thenReturn(servletContext);
+    when(servletContext.getAttribute(AppContextListener.ATTR_TASK_QUEUE)).thenReturn(queue);
+
+    // Prepare data storage
+    when(request.getServletContext()).thenReturn(servletContext);
+    when(servletContext.getAttribute(AppContextListener.ATTR_DATA_STORAGE)).thenReturn(dataStorage);
+    var printWriter = mock(PrintWriter.class);
+    when(response.getWriter()).thenReturn(printWriter);
+    doNothing().when(printWriter).write(anyString());
+    servlet.doPost(request, response);
+    verify(response).sendError(SC_BAD_REQUEST, "Missing right hand side in request");
+  }
+
+  @Test
+  void testDoPostNoSolvingMethod() throws ServletException, IOException {
+    // Prepare matrix of SLAE
+    var expectedMatrix =
+        new String[][] {{"1.3", "2.2", "3.7"}, {"7.7", "2.1", "9.3"}, {"1.1", "4.8", "2.3"}};
+    var sb = new StringBuilder();
+    for (var row : expectedMatrix) {
+      sb.append(String.join(",", row));
+      sb.append(System.lineSeparator());
+    }
+    var isMatrix = new ByteArrayInputStream(sb.toString().getBytes(UTF_8));
+    var partMatrix = mock(Part.class);
+    when(partMatrix.getInputStream()).thenReturn(isMatrix);
+    when(request.getPart("matrix")).thenReturn(partMatrix);
+
+    // Prepare right hand side of SLAE
+    var expectedRhs = new String[] {"1.3", "2.2", "3.7"};
+    var isRhs =
+        new ByteArrayInputStream(String.join(System.lineSeparator(), expectedRhs).getBytes(UTF_8));
+    var partRhs = mock(Part.class);
+    when(partRhs.getInputStream()).thenReturn(isRhs);
+    when(request.getPart("rhs")).thenReturn(partRhs);
+
+    // Prepare queue
+    when(request.getServletContext()).thenReturn(servletContext);
+    when(servletContext.getAttribute(AppContextListener.ATTR_TASK_QUEUE)).thenReturn(queue);
+
+    // Prepare data storage
+    when(request.getServletContext()).thenReturn(servletContext);
+    when(servletContext.getAttribute(AppContextListener.ATTR_DATA_STORAGE)).thenReturn(dataStorage);
+    var printWriter = mock(PrintWriter.class);
+    when(response.getWriter()).thenReturn(printWriter);
+    doNothing().when(printWriter).write(anyString());
+    servlet.doPost(request, response);
+    verify(response).sendError(SC_BAD_REQUEST, "Invalid solving method: null");
+  }
+
+  @Test
+  void testDoPostNoQueue() throws ServletException, IOException {
+    // Prepare matrix of SLAE
+    var expectedMatrix =
+        new String[][] {{"1.3", "2.2", "3.7"}, {"7.7", "2.1", "9.3"}, {"1.1", "4.8", "2.3"}};
+    var sb = new StringBuilder();
+    for (var row : expectedMatrix) {
+      sb.append(String.join(",", row));
+      sb.append(System.lineSeparator());
+    }
+    var isMatrix = new ByteArrayInputStream(sb.toString().getBytes(UTF_8));
+    var partMatrix = mock(Part.class);
+    when(partMatrix.getInputStream()).thenReturn(isMatrix);
+    when(request.getPart("matrix")).thenReturn(partMatrix);
+
+    // Prepare right hand side of SLAE
+    var expectedRhs = new String[] {"1.3", "2.2", "3.7"};
+    var isRhs =
+        new ByteArrayInputStream(String.join(System.lineSeparator(), expectedRhs).getBytes(UTF_8));
+    var partRhs = mock(Part.class);
+    when(partRhs.getInputStream()).thenReturn(isRhs);
+    when(request.getPart("rhs")).thenReturn(partRhs);
+
+    // Prepare solving method
+    when(request.getParameter("slaeSolvingMethod")).thenReturn("numpy_exact_solver");
+
+    // Prepare data storage
+    when(request.getServletContext()).thenReturn(servletContext);
+    when(servletContext.getAttribute(AppContextListener.ATTR_DATA_STORAGE)).thenReturn(dataStorage);
+    var printWriter = mock(PrintWriter.class);
+    when(response.getWriter()).thenReturn(printWriter);
+    doNothing().when(printWriter).write(anyString());
+    servlet.doPost(request, response);
+    verify(response).sendError(SC_INTERNAL_SERVER_ERROR, "Service Queue is not initialized");
+  }
+
+  @Test
+  void testDoPostNoDataStorage() throws ServletException, IOException {
+    // Prepare matrix of SLAE
+    var expectedMatrix =
+        new String[][] {{"1.3", "2.2", "3.7"}, {"7.7", "2.1", "9.3"}, {"1.1", "4.8", "2.3"}};
+    var sb = new StringBuilder();
+    for (var row : expectedMatrix) {
+      sb.append(String.join(",", row));
+      sb.append(System.lineSeparator());
+    }
+    var isMatrix = new ByteArrayInputStream(sb.toString().getBytes(UTF_8));
+    var partMatrix = mock(Part.class);
+    when(partMatrix.getInputStream()).thenReturn(isMatrix);
+    when(request.getPart("matrix")).thenReturn(partMatrix);
+
+    // Prepare right hand side of SLAE
+    var expectedRhs = new String[] {"1.3", "2.2", "3.7"};
+    var isRhs =
+        new ByteArrayInputStream(String.join(System.lineSeparator(), expectedRhs).getBytes(UTF_8));
+    var partRhs = mock(Part.class);
+    when(partRhs.getInputStream()).thenReturn(isRhs);
+    when(request.getPart("rhs")).thenReturn(partRhs);
+
+    // Prepare solving method
+    when(request.getParameter("slaeSolvingMethod")).thenReturn("numpy_exact_solver");
+
+    // Prepare queue
+    when(request.getServletContext()).thenReturn(servletContext);
+    when(servletContext.getAttribute(AppContextListener.ATTR_TASK_QUEUE)).thenReturn(queue);
+
+    var printWriter = mock(PrintWriter.class);
+    when(response.getWriter()).thenReturn(printWriter);
+    doNothing().when(printWriter).write(anyString());
+    servlet.doPost(request, response);
+    verify(response).sendError(SC_INTERNAL_SERVER_ERROR, "Data Storage Service is not initialized");
   }
 }
