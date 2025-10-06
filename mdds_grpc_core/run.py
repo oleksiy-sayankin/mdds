00ca6c46cd91e6687c5dd655b47766f20a80a9cf
Oleksiy Oleksandrovych Sayankin <oleksiy.sayankin@gmail.com>
2025-10-06 13:29:30 +0300

    MDDS-155 Remove FastAPI web server from gRPC server

--- mdds_grpc_core/run.py
+++ mdds_grpc_core/run.py
@@ -3,76 +3,21 @@
 """
 Entry point for running gRPC web server with chosen solver
 """
-import os
+
 import asyncio
-
-import uvicorn
-from contextlib import asynccontextmanager
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from grpcserver import GrpcServer
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
-
-grpc_webclient_dir = os.path.join(os.path.dirname(__file__), "grpc_webclient")
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    """Here we start and stop gRPC Server"""
-    asyncio.create_task(GrpcServer().start())
+async def main():
+    """Entry point: start and gracefully stop gRPC server."""
+    server = GrpcServer()
     try:
-        yield
+        await server.start()
+    except KeyboardInterrupt:
+        pass
     finally:
-        await GrpcServer().stop()
-
-
-app = FastAPI(
-    lifespan=lifespan,
-    title="gRPC service on Python",
-    description="Solves SLAE using gRPC on Python",
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.mount(
-    "/static", StaticFiles(directory=grpc_webclient_dir, html=True), name="static"
-)
-
-
-@app.get("/")
-def index():
-    """
-    Root endpoint.
-
-    :return: gRPC web server page index.html
-    """
-    return FileResponse(os.path.join(grpc_webclient_dir, "index.html"))
-
-
-@app.get("/health")
-def health():
-    """
-    Health check for gRPC web server
-
-    :return: always returns ok as health status
-    """
-    return {"status": "ok"}
+        await server.stop()
 
 
 if __name__ == "__main__":
-    grpc_webserver_host = os.getenv("MDDS_EXECUTOR_GRPC_WEBSERVER_HOST", "localhost")
-    grpc_webserver_port = os.getenv("MDDS_EXECUTOR_GRPC_WEBSERVER_PORT", 40062)
-
-    uvicorn.run(
-        "run:app",
-        host=grpc_webserver_host,
-        port=grpc_webserver_port,
-        reload=True,
-    )
+    asyncio.run(main())
