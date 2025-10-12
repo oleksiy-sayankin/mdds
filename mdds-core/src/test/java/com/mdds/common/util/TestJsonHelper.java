f3d3b538e8a54343b0972e11fdbbf895510eec28
Oleksiy Oleksandrovych Sayankin <oleksiy.sayankin@gmail.com>
2025-10-12 19:31:12 +0300

    MDDS-184 Add JUnits on DTO objects

--- mdds-core/src/test/java/com/mdds/common/util/TestJsonHelper.java
+++ mdds-core/src/test/java/com/mdds/common/util/TestJsonHelper.java
@@ -7,7 +7,10 @@ package com.mdds.common.util;
 import static com.mdds.dto.SlaeSolver.NUMPY_EXACT_SOLVER;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.mdds.dto.ResultDTO;
 import com.mdds.dto.TaskDTO;
+import com.mdds.dto.TaskIdResponseDTO;
+import com.mdds.dto.TaskStatus;
 import java.time.Instant;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -29,6 +32,64 @@ class TestJsonHelper {
     var expectedJson =
         "{\"id\":\"testId\",\"dateTime\":1725466860.000000000,\"matrix\":[[2.2,3.3],[4.5,6.3]],\"rhs\":[4.7,8.9],\"slaeSolvingMethod\":\"NUMPY_EXACT_SOLVER\"}";
     Assertions.assertEquals(expectedJson, actualJson);
+  }
+
+  @Test
+  void testToJsonResultDto() {
+    var taskId = "testId";
+    var isoFormattedString = "2024-09-04T16:21:00Z";
+    var instant = Instant.parse(isoFormattedString);
+    var resultDTO =
+        new ResultDTO(
+            taskId, instant, instant, TaskStatus.DONE, new double[] {1.971, 3.213, 7.243}, "");
+    var actualJson = JsonHelper.toJson(resultDTO);
+    var expectedJson =
+        "{"
+            + "\"taskId\":\"testId\","
+            + "\"dateTimeTaskCreated\":1725466860.000000000,"
+            + "\"dateTimeTaskFinished\":1725466860.000000000,"
+            + "\"taskStatus\":\"DONE\","
+            + "\"solution\":[1.971,3.213,7.243],"
+            + "\"errorMessage\":\"\""
+            + "}";
+    Assertions.assertEquals(expectedJson, actualJson);
+  }
+
+  @Test
+  void testFromJsonResultDto() {
+    var json =
+        "{"
+            + "\"taskId\":\"testId\","
+            + "\"dateTimeTaskCreated\":1725466860.000000000,"
+            + "\"dateTimeTaskFinished\":1725466860.000000000,"
+            + "\"taskStatus\":\"DONE\","
+            + "\"solution\":[1.971,3.213,7.243],"
+            + "\"errorMessage\":\"\""
+            + "}";
+    var actualResult = JsonHelper.fromJson(json, ResultDTO.class);
+    var taskId = "testId";
+    var isoFormattedString = "2024-09-04T16:21:00Z";
+    var instant = Instant.parse(isoFormattedString);
+    var expectedResult =
+        new ResultDTO(
+            taskId, instant, instant, TaskStatus.DONE, new double[] {1.971, 3.213, 7.243}, "");
+    Assertions.assertEquals(expectedResult, actualResult);
+  }
+
+  @Test
+  void testToJsonTaskIdResponseDTO() {
+    var taskIdResponse = new TaskIdResponseDTO("87a027b0-beb7-4171-8fbf-7b7568dce461");
+    var actualJson = JsonHelper.toJson(taskIdResponse);
+    var expectedJson = "{\"id\":\"87a027b0-beb7-4171-8fbf-7b7568dce461\"}";
+    Assertions.assertEquals(expectedJson, actualJson);
+  }
+
+  @Test
+  void testFromJsonTaskIdResponseDTO() {
+    var json = "{\"id\":\"87a027b0-beb7-4171-8fbf-7b7568dce461\"}";
+    var expectedTaskIdResponseDTO = new TaskIdResponseDTO("87a027b0-beb7-4171-8fbf-7b7568dce461");
+    var actualTaskIdResponseDTO = JsonHelper.fromJson(json, TaskIdResponseDTO.class);
+    Assertions.assertEquals(expectedTaskIdResponseDTO, actualTaskIdResponseDTO);
   }
 
   @Test
